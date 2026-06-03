e4a817e5c4311d82a52b6f169a1b013ace091b85
TechStore Student <student@techstore.com>
2026-06-04 01:31:20 +0700

    Fix typescript build error by removing invalid _id schema property

--- models/Product.ts
+++ models/Product.ts
@@ -58,7 +58,6 @@ export interface IProduct extends Document {
 
 const ProductSchema = new Schema<IProduct>(
   {
-    _id: { type: String, required: true },
     name: { type: String, required: true, index: true },
     slug: { type: String, required: true, unique: true, index: true },
     category: {
